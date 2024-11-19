cff48bc6b740d22f7a1ce634ae82c8b7f63bd405
balakrishnanair <balakrishna2352k@gmail.com>
2024-11-19 12:09:10 -0500

    profile page 19th

--- classnest/classnest_Base/models.py
+++ classnest/classnest_Base/models.py
@@ -1,6 +1,7 @@
 # classnest_Base/models.py
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
@@ -13,23 +14,16 @@ class Course(models.Model):
         return self.title
 
 
+
 class Profile(models.Model):
     USER_TYPE_CHOICES = (
         ('student', 'Student'),
         ('instructor', 'Instructor'),
     )
 
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
-    # bio = models.TextField(max_length=500, blank=True)
-    # contact = models.CharField(max_length=100, blank=True)
-    # links = models.URLField(blank=True)
-
-    # def __str__(self):
-    #     return f"{self.user.username} - {self.user_type}"
-    
-    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now, auto_now_add=True)
     contact = models.CharField(max_length=100, blank=True)
     github = models.URLField(blank=True)  # GitHub field
     linkedin = models.URLField(blank=True)  # LinkedIn field
